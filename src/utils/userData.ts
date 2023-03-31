ba5ac542c9610aac6fceebf5a02293d98ba6d217
FelipeGK <felipegrondek00@gmail.com>
2023-03-31 10:00:51 -0300

    first commit

---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Felipe Grondek",
+  githubUser: "Felipe-Grondek",
+  linkedinUser: "felipegrondek",
+  whatsappNumber: "+5566996932537",
+  emailUser: "felipegrondek00@gmail.com",
+  instagramUser: "felps.gk",
+  facebookUser: "",
+};
